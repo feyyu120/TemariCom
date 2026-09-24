1f8205b778b9cc63b9abdf7c12085fdc8f03d5b5
feyyu120 <feyselyassin18@gmail.com>
2026-09-24 16:57:15 +0300

    feat(web/profiles): export profile UI components from components index

---
+++ web/src/features/profiles/components/index.ts
@@ -0,0 +1,6 @@
+export * from '@/features/profiles/components/ProfileSkeleton';
+export * from '@/features/profiles/components/ProfileHeader';
+export * from '@/features/profiles/components/AcademicInfoCards';
+export * from '@/features/profiles/components/EditProfileModal';
+export * from '@/features/profiles/components/ProfileSettingsModal';
+
