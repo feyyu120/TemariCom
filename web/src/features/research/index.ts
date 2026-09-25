f79bf6bc77574319b87c484fe52910cfbefd1a33
feyyu120 <feyselyassin18@gmail.com>
2026-09-25 18:30:54 +0300

    export public research module interface

---
+++ web/src/features/research/index.ts
@@ -0,0 +1,5 @@
+export * from '@/features/research/types';
+export * from '@/features/research/services/researchService';
+export * from '@/features/research/hooks';
+export * from '@/features/research/components';
+export { ResearchPage } from '@/features/research/pages/ResearchPage';
