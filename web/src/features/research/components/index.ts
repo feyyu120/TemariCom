074d566cfb3074054cfa201ffb49b74bbf1e83f9
feyyu120 <feyselyassin18@gmail.com>
2026-09-25 18:30:40 +0300

    add research components barrel export

---
+++ web/src/features/research/components/index.ts
@@ -0,0 +1,3 @@
+export { PaperSkeleton } from '@/features/research/components/PaperSkeleton';
+export { ResearchSearchBar } from '@/features/research/components/ResearchSearchBar';
+export { PaperCard } from '@/features/research/components/PaperCard';
