39e7a3a9dc67d637b74e7b6ba5bbdea7e6f60ab8
feyyu120 <feyselyassin18@gmail.com>
2026-09-25 18:30:07 +0300

    add research hooks barrel export

---
+++ web/src/features/research/hooks/index.ts
@@ -0,0 +1,3 @@
+export { usePaperSearch } from '@/features/research/hooks/usePaperSearch';
+export { useSavedPapers } from '@/features/research/hooks/useSavedPapers';
+export { useSavePaper } from '@/features/research/hooks/useSavePaper';
